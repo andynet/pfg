a1db9d7ef68cca4126a22e899b0a0cfcebe0e1d3
Andrej Balaz <andrejbalaz001@gmail.com>
2024-06-27 19:03:47 +0200

    added expansions

--- src/bin/triggers_matrix_sorted.rs
+++ src/bin/triggers_matrix_sorted.rs
@@ -1,21 +1,27 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use core::panic;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::fs::File;
 use std::io::stdin;
+use std::io::stdout;
 use std::io::BufWriter;
 use std::io::Write;
 use bio::io::fasta;
 use itertools::Itertools;
 use std::env;
+use std::str;
 
-// const ALPH_SIZE: usize = 4;
+const ALPH: usize = 4;
+type KMap = HashMap<Vec<u8>, Box<[u32; ALPH.pow(2)]>>;
 
 fn main() {
     let k: usize = env::args().nth(1).unwrap().parse().unwrap();
     let outfile: String = env::args().nth(2).unwrap();
 
-    let mut kmap: HashMap<Vec<u8>, Box<[u32; 16]>> = HashMap::new();
+    let mut kmap: KMap = HashMap::new();
     let mut records = fasta::Reader::new(stdin()).records();
     while let Some(Ok(record)) = records.next() {
         let mut seq = record.seq().to_owned();
@@ -23,31 +29,159 @@ fn main() {
         add_counts(&mut kmap, &seq, k);
     }
 
-    let mut heatmap = [[0; 16];16];
-    for v in kmap.values() {
-        let nzeros = *v.iter().counts().get(&0).unwrap_or(&0);
-        let value = measure(v);
-        heatmap[nzeros][ftointerval(value)] += 1;
-    }
-    print_heatmap(&heatmap);
+    // print_heatmap(&kmap);
+    // let out = BufWriter::new(File::create(outfile).unwrap());
+    // save_results(&kmap, out);
+    let clean: KMap = kmap.into_iter().filter(|x| {measure(&x.1) == 1.0}).collect();
+    println!("{}", clean.len());
+}
 
+#[test]
+fn find_chains() {
+    let seqs = [
+        b"ACGTGTGCACGTGCAGATGCTTAGCTTAGCTTAGCCCTA",
+        b"ACGTGTGCACGTGCAGATGCTTAGCTTAGCTTAGCCCTA",
+        b"ACGTGTGCACGTGCAGATGCTTAGCTTAGCTTAGCCCTA",
+//                          GCTTAGC   GCTTAGC
+//                               GCTTAGC
+//                     CAGATG
+//               CACGT
+    ];
+    let k = 2;
+
+    let mut kmap = HashMap::new();
+    for seq in seqs {
+        add_counts(&mut kmap, seq, k);
+    }
+    // print_heatmap(&kmap);
+    save_results(&kmap, stdout());
+    let mut kmap: KMap = kmap.into_iter().filter(|x| {measure(&x.1) == 1.0}).collect();
+
+    // let x = kmap.iter().next().unwrap().0.clone();
+    let kmap2 = kmap.clone();
+    for (key, v) in &kmap2 {
+        let nzeros = *v.iter().counts().get(&0).unwrap_or(&0);
+        if nzeros != 15 { continue; }
+
+        let kmer = key.to_vec();
+        let context: (u8, u8) = get_singleton_context(v);
+        let n_occ: u32 = kmap.get(&kmer).unwrap().iter().sum();
+
+        let (rkmer, rlen) = expand_right(&mut kmap, &kmer, context, 1);
+        let (lkmer, llen) = expand_left( &mut kmap, &kmer, context, 1);
+        println!("{} => {}\t{}\t{}\t{}",
+            str::from_utf8(&kmer).unwrap(),
+            str::from_utf8(&lkmer).unwrap(),
+            str::from_utf8(&rkmer).unwrap(), 
+            rlen + llen + k,
+            n_occ
+        );
+    }
+}
+
+fn get_singleton_context(contexts: &[u32; 16]) -> (u8, u8) {
+    let pos = contexts.iter().find_position(|&x| *x != 0).unwrap().0;
+    let row = pos / 4;
+    let col = pos % 4;
+    let left = match row {
+        0 => b'A', 1 => b'C', 2 => b'G', 3 => b'T', _ => panic!("Weird context")
+    };
+    let right = match col {
+        0 => b'A', 1 => b'C', 2 => b'G', 3 => b'T', _ => panic!("Weird context")
+    };
+    return (left, right);
+}
+
+fn expand_right(
+    kmap: &mut KMap, kmer: &[u8], context: (u8, u8), len: usize
+) -> (Vec<u8>, usize) {
+    let mut new_kmer = kmer[1..].to_vec();
+    new_kmer.push(context.1);
+
+    match kmap.entry(new_kmer.clone()) {
+        Entry::Vacant(x) => { return (new_kmer.to_vec(), len) }
+        Entry::Occupied(mut x) => {
+            let context = x.get_mut();
+            let after = get_right_context(context, kmer[0]);
+            let new_context = (kmer[0], after);
+            return expand_right(kmap, &new_kmer, new_context, len + 1);
+        }
+    }
+}
+
+fn get_right_context(contexts: &[u32; 16], left: u8) -> u8 {
+    let row = match left {
+        b'A' => 0, b'C' => 1, b'G' => 2, b'T' => 3, _ => panic!("Weird context")
+    };
+    let mut col = 4;
+    for j in 0..4 { if contexts[4*row + j] != 0 { col = j; } }
+    let right = match col {
+        0 => b'A', 1 => b'C', 2 => b'G', 3 => b'T', _ => panic!("Weird context")
+    };
+    return right;
+}
+
+fn expand_left(
+    kmap: &mut KMap, kmer: &[u8], context: (u8, u8), len: usize
+) -> (Vec<u8>, usize) {
+    let k = kmer.len();
+    let mut new_kmer = vec![context.0];
+    new_kmer.extend(&kmer[..k-1]);
+
+    match kmap.entry(new_kmer.clone()) {
+        Entry::Vacant(x) => { return (new_kmer.to_vec(), len) }
+        Entry::Occupied(mut x) => {
+            let context = x.get_mut();
+            let before = get_left_context(context, kmer[k-1]);
+            let new_context = (before, kmer[k-1]);
+            return expand_left(kmap, &new_kmer, new_context, len + 1);
+        }
+    }
+}
+
+fn get_left_context(contexts: &[u32; 16], right: u8) -> u8 {
+    let col = match right {
+        b'A' => 0, b'C' => 1, b'G' => 2, b'T' => 3, _ => panic!("Weird context")
+    };
+    let mut row = 4;
+    for j in 0..4 { if contexts[4*j + col] != 0 { row = j; } }
+    let left = match row {
+        0 => b'A', 1 => b'C', 2 => b'G', 3 => b'T', _ => panic!("Weird context")
+    };
+    return left;
+}
+
+
+// fn expand_chain(kmap: &mut HashMap<>, kmer: &[u8]) -> (left, right, ch_len, n_occ) {
+//     let right = expand_right(kmap: &mut HashMap<>, kmer: &[u8]);
+//     let left = expand_left(kmap: &mut HashMap<>, kmer: &[u8]);
+//     return (left, right, ch_len, n_occ);
+// }
+
+fn save_results(kmap: &KMap, mut out: impl Write) {
     let mut result = Vec::with_capacity(kmap.len());
     for (k, v) in kmap {
         let nzeros = *v.iter().counts().get(&0).unwrap_or(&0);
-        let value = measure(&v);
+        let value = measure(v);
         result.push((value, nzeros, k, v));
     }
     result.sort_by(|a, b| a.0.total_cmp(&b.0));
-    let mut out = BufWriter::new(File::create(outfile).unwrap());
     for (val, nz, k, v) in result {
-        write!(out, "{}\t", std::str::from_utf8(&k).unwrap()).unwrap();
+        write!(out, "{}\t", std::str::from_utf8(k).unwrap()).unwrap();
         write!(out, "{:.4}\t", val).unwrap();
         write!(out, "{}\t", nz).unwrap();
         writeln!(out, "{:?}", v).unwrap();
     }
 }
 
-fn print_heatmap(heatmap: &[[i32; 16]; 16]) {
+fn print_heatmap(kmap: &KMap) {
+    let mut heatmap = [[0; 16];16];
+    for v in kmap.values() {
+        let nzeros = *v.iter().counts().get(&0).unwrap_or(&0);
+        let value = measure(v);
+        heatmap[nzeros][ftointerval(value)] += 1;
+    }
+
     let sum: i32 = heatmap.iter()
         .map(|x| -> i32 { x.iter().sum() })
         .sum();
@@ -71,12 +205,6 @@ fn print_heatmap(heatmap: &[[i32; 16]; 16]) {
     }
     println!();
 }
-
-// fn expand_chain(kmap: &mut HashMap<>, kmer: &[u8]) -> (left, right, ch_len, n_occ) {
-//     let right = expand_right(kmap: &mut HashMap<>, kmer: &[u8]);
-//     let left = expand_left(kmap: &mut HashMap<>, kmer: &[u8]);
-//     return (left, right, ch_len, n_occ);
-// }
 
 fn add_counts(kmap: &mut HashMap<Vec<u8>, Box<[u32; 16]>>, seq: &[u8], k: usize) {
     for i in 1..seq.len()-k-1 {
